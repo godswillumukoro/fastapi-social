a81d644cc30935e05655bf78577aaac36689ff56
Godswill Umukoro <umukoro6@gmail.com>
2024-02-22 12:48:58 +0100

    set common prefix for all endpoints

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,16 +4,18 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 from typing import Optional, List
 
-router = APIRouter()
+router = APIRouter(
+    prefix = '/posts'
+)
 
-@router.get('/posts', response_model=List[schemas.PostResponse])
+@router.get('/', response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).all()
     return posts
 
-@router.get("/posts/latest", response_model=List[schemas.PostResponse])
+@router.get("/latest", response_model=List[schemas.PostResponse])
 def get_latest_post():
     try:
         cursor.execute(""" SELECT * FROM posts ORDER BY posts.id DESC LIMIT 2 """)
@@ -27,7 +29,7 @@ def get_latest_post():
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
-@router.get("/posts/{id}", response_model=schemas.PostResponse)
+@router.get("/{id}", response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)): # path operator function
     try:
         # cursor.execute(""" SELECT * FROM posts WHERE id= %s """, (id,))
@@ -42,7 +44,7 @@ def get_post(id: int, db: Session = Depends(get_db)): # path operator function
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@router.post("/posts", status_code = status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+@router.post("/", status_code = status.HTTP_201_CREATED, response_model=schemas.PostResponse)
 def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute(""" INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.is_published))
 
@@ -58,7 +60,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
     return new_post
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     try:
         # cursor.execute(""" DELETE FROM posts WHERE id= %s RETURNING * """, (id,))
@@ -78,7 +80,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@router.put("/posts/{id}", response_model=schemas.PostResponse)
+@router.put("/{id}", response_model=schemas.PostResponse)
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     try:
         # cursor.execute(""" UPDATE posts SET title = %s, content = %s, is_published = %s WHERE id = %s RETURNING * """,
